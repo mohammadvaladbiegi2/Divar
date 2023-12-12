2a1bab9c1615807a2c4b9a82ac08757aaf998741
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2023-12-12 10:41:35 +0330

    add loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { useRoutes } from "react-router-dom";
 import routs from "./routs";
 import "./App.css";
 import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import Loader from "./componante/loder/Loader";
 
 function App() {
   const rout = useRoutes(routs);
 
+  const [load, setload] = useState(false);
+
   return (
-    <>
-      {rout}
+    <div onLoad={() => setload(true)}>
+      {load ? rout : <Loader />}
       <Toaster position="top-right" />
-    </>
+    </div>
   );
 }
 
