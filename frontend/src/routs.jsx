831126a5e776f2d8ac1a185b2b65f34c3d7c0519
mohammadvaladbiegi2 <126916758+mohammadvaladbiegi2@users.noreply.github.com>
2023-12-12 14:05:03 +0330

    add lazy load

--- frontend/src/routs.jsx
+++ frontend/src/routs.jsx
@@ -1,9 +1,19 @@
+import React from "react";
 import Newproduct from "./pages/Newproduct/Newproduct";
 import Homepage from "./pages/homepage/homepage";
 import Mainproductpage from "./pages/productpage/productpage";
+import Loader from "./componante/loder/Loader";
+const Lazyhopage = React.lazy(() => import("./pages/homepage/homepage"));
 
 const routs = [
-  { path: "/", element: <Homepage /> },
+  {
+    path: "/",
+    element: (
+      <React.Suspense fallback={<Loader />}>
+        <Lazyhopage />
+      </React.Suspense>
+    ),
+  },
   { path: "/newproduct", element: <Newproduct /> },
   { path: "/product/:shortname", element: <Mainproductpage /> },
 ];
